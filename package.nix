80f7411d1f51d033a3a1b2d2a15bac3e06e81e5c
Walker <walker@praiseweb.com.br>
2022-09-21 16:37:06 -0300

    Use lovelaceAcademy only deps

--- package.nix
+++ package.nix
@@ -1,5 +1,5 @@
-{ ps-pkgs, ps-pkgs-ns, ... }:
-with ps-pkgs;
-with ps-pkgs-ns; {
-  dependencies = [ prelude console lovelaceAcademy.properties ];
+{ ps-pkgs-ns, ... }:
+with ps-pkgs-ns.lovelaceAcademy;
+{
+  dependencies = [ prelude console properties ];
 }
